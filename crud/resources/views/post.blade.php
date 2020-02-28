d8feb2a408769503e44993737cf686535bf1fecd
kelvinrandu <randukelvin@gmail.com>
2020-02-28 18:28:24 +0300

    added laravel passport

--- crud/resources/views/post.blade.php
+++ crud/resources/views/post.blade.php
@@ -9,9 +9,8 @@
         <meta name="csrf-token" value="{{ csrf_token() }}" />
     </head>
     <body>
-        <div id="app">
-          <example-component></example-component>
-        </div>
-        <script src="{{ mix('js/app.js') }}" type="text/javascript"></script>
+      <div id="app">
+      </div>
+      <script src="{{ mix('js/app.js') }}" type="text/javascript"></script>
     </body>
 </html>
--- crud/routes/web.php
+++ crud/routes/web.php
@@ -15,3 +15,7 @@
 Route::get('/{any}', function () {
     return view('post');
   })->where('any', '.*');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
